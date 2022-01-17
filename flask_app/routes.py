e75689dd88b1adccd8b37134db7399007d782e2e
Wesley Chen <wesleychen44@gmail.com>
2022-01-16 19:33:55 -0800

    Mostly Finished

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -10,6 +10,22 @@ def index():
 def projects():
     return render_template("projects.html")
 
+@app.route("/contact", methods=["GET"])
+def contact():
+    return render_template("contact.html")
 
+@app.route("/projects/data_science", methods=["GET"])
+def data_science():
+    return render_template("project_320.html")
 
+@app.route("/projects/professor", methods=["GET"])
+def professor():
+    return render_template("project_professor.html")
 
+@app.route("/projects/initium", methods=["GET"])
+def initium():
+    return render_template("project_initium.html")
+
+@app.route("/projects/settlement", methods=["GET"])
+def settlement():
+    return render_template("project_settlement.html")
